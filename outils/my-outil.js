c2b45179ac3c5b0d28c11e98e2bc64690e9bd642
lynn <heling19910922@163.com>
2018-01-15 11:43:44 +0800

    no message

--- outils/my-outil.js
+++ outils/my-outil.js
@@ -155,12 +155,14 @@ function duplicates2(arr) {
 }
 
 /*
-css 中经常有类似 background-image 这种通过 - 连接的字符，通过 javascript 设置样式的时候需要将这种样式转换成 backgroundImage 驼峰格式，请完成此转换功能
-1. 以 - 为分隔符，将第二个起的非空单词首字母转为大写
-2. -webkit-border-image 转换后的结果为 webkitBorderImage
+字符串字符统计
+统计字符串中每个字符的出现频率，返回一个 Object，key 为统计字符，value 为出现频率
+1. 不限制 key 的顺序
+2. 输入的字符串参数不会为空
+3. 忽略空白字符
 */
 function count(str) {
-	var strArr = str.split("");
+  var strArr = str.split("");
     var obj = {};
     for(var i=0;i<strArr.length;i++){
         if(strArr[i] == " "){
@@ -174,6 +176,63 @@ function count(str) {
     }
     return obj;
 }
+
+/*
+斐波那契数列
+用 JavaScript 实现斐波那契数列函数,返回第n个斐波那契数。 f(1) = 1, f(2) = 1 等
+*/
+function fibonacci(n) {
+    if(n==1 || n==2){
+        return 1;
+    }else{
+        return fibonacci(n-2)+fibonacci(n-1);
+    }
+}
+
+/*
+css 中经常有类似 background-image 这种通过 - 连接的字符，通过 javascript 设置样式的时候需要将这种样式转换成 backgroundImage 驼峰格式，请完成此转换功能
+1. 以 - 为分隔符，将第二个起的非空单词首字母转为大写
+2. -webkit-border-image 转换后的结果为 webkitBorderImage
+*/
+function cssStyle2DomStyle(sName) {
+  var sArr = sName.split("-");
+    var result = "";
+    for(var i=0;i<sArr.length;i++){
+        if(i == 0 && sArr[i] || (i == 1 && !sArr[0])){
+            result = sArr[i];
+        }else{
+             result += sArr[i].slice(0,1).toUpperCase() + sArr[i].slice(1);
+        }
+    }
+    return result;
+}
+
+/*
+颜色字符串转换
+将 rgb 颜色字符串转换为十六进制的形式，如 rgb(255, 255, 255) 转为 #ffffff
+1. rgb 中每个 , 后面的空格数量不固定
+2. 十六进制表达式使用六位小写字母
+3. 如果输入不符合 rgb 格式，返回原始输入
+*/
+function rgb2hex(sRGB) {
+    var regexp=/rgb\((\d+),\s*(\d+),\s*(\d+)\)/;
+    var ret=sRGB.match(regexp);
+    if(!ret){
+        return sRGB;
+    }else{
+        var str='#';
+        for(var i=1;i<=3;i++){
+            var m=parseInt(ret[i]);
+            if(m<=255&&m>=0){
+                str+=(m<16?'0'+m.toString(16):m.toString(16));
+            }else{
+                return sRGB;
+            }
+        }
+        return str;
+    }
+}
+
 
 /*
 查找两个节点的最近的一个共同父节点，可以包括节点自身
